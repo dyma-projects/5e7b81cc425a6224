0ecc87197b2ed790341c9389d6acec1aee27a428
junglewilly <guillaumec.dev@gmail.com>
2020-05-02 18:37:16 +0200

    exercice 1 done

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  public result: number = 0;
+  @Input() public compteur: number;
+  @Output() public sendResult: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.compteur = 0;
   }
 
+  increment(): void {
+    this.result++;
+    this.sendResult.emit(this.result);
+  }
+
+  decrement(): void {
+    this.result--;
+    this.sendResult.emit(this.result);
+  }
 }
